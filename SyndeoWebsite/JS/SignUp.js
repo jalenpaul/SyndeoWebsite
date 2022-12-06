dfc8d8bc498ef029b8cfd132a802367464b3bf1d
jalenpaul <38700576+jalenpaul@users.noreply.github.com>
2022-12-06 14:08:31 -0800

    Update SignUp.js

--- SyndeoWebsite/JS/SignUp.js
+++ SyndeoWebsite/JS/SignUp.js
@@ -154,7 +154,8 @@ function insertUser() {
             pfpURL: userModel.pfpURL,
             username: userModel.username,
         }).then(() => {
-            //TODo send user to Home page
+            sessionStorage.setItem("userID", userModel.userID);
+            window.open("https://jalenpaul.github.io/SyndeoWebsite/SyndeoWebsite/HTML/CompleteProfile", "_self");
             resolve();
         }).catch((error) => {
             alert("Error ocurred while creating profile.");
